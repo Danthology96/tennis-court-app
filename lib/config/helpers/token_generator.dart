787fabd957619ef1d867a7965903f86f44d1e250
Daniel Novillo <82913698+Danthology96@users.noreply.github.com>
2024-08-29 08:51:02 -0500

    🧪 Added jwt test
    🧪 Added auth tests including jwt

--- lib/config/helpers/token_generator.dart
+++ lib/config/helpers/token_generator.dart
@@ -1,23 +1,29 @@
-import 'dart:convert';
-// ignore: depend_on_referenced_packages
-import 'package:crypto/crypto.dart' as crypto;
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// function to create a token for the user based on the user's email and password
-String createToken({required String email, required String password}) {
-  // Concatenate the email and password
-  String data = email + password;
+Future<String> createToken(
+    {required String email, required String password}) async {
+  await dotenv.load(fileName: ".env");
+  final secretKey = dotenv.env['JWT_SECRET_KEY'] ?? 'default-secret-key';
+  // Create a JWT with the email and password as payload
+  final jwt = JWT({
+    'email': email,
+    'password': password,
+  });
 
-  // Create a SHA256 hash of the concatenated data
-  var bytes = utf8.encode(data);
-  var digest = crypto.sha256.convert(bytes);
+  // Sign the JWT with a secret key
+  final token = jwt.sign(SecretKey(secretKey));
 
-  // Return the hexadecimal representation of the hash
-  return digest.toString();
+  return token;
 }
 
 /// function to decode the token
-String decodeToken({required String token}) {
-  // Decode the token
-  var decoded = utf8.decode(base64.decode(token));
-  return decoded;
+Future<Map<String, dynamic>> decodeToken({required String token}) async {
+  await dotenv.load(fileName: ".env");
+  final secretKey = dotenv.env['JWT_SECRET_KEY'] ?? 'default-secret-key';
+  // Verify and decode the JWT
+  final jwt = JWT.verify(token, SecretKey(secretKey));
+
+  return jwt.payload;
 }
